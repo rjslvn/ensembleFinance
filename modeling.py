76064238dae1f8b78fd0db767bd1cb6661f550ae
rjslvn <rjslvn.nv@gmail.com>
2023-06-08 22:48:31 -0700

    added more epochs and future weeks"

--- modeling.py
+++ modeling.py
@@ -98,42 +98,44 @@ def main():
     plt.show()
 
     # Predicting for next 26 weeks (approx. rest of 2023)
-    future_weeks = 52
-    forecast = []
+    # Number of future weeks we want to predict
+    # Number of future weeks we want to predict
+    future_weeks = 26
 
-    current_week = testX[-1:].tolist()
+    # Getting the last known window of data
+    last_window = testX[-1].unsqueeze(0)
+
+    # Preparing an array to hold our future predictions
+    future_pred = np.empty(future_weeks)
+
+    # Iteratively predict the next week
     for i in range(future_weeks):
-        with torch.no_grad():
-            model.eval()
-            current_week_var = Variable(torch.Tensor(current_week))
-            predict = model(current_week_var)
-            current_week.append(predict.item())
-            current_week.pop(0)
-            forecast.append(predict.item())
+        # Predict the next week
+        next_week = model(last_window)
+        future_pred[i] = next_week.detach().numpy()
 
-    forecast = scaler.inverse_transform(np.array(forecast).reshape(-1,1))
+        # Update the "last window" to include the new prediction
+        last_window = torch.cat((last_window[0,1:], next_week.unsqueeze(0)), 0).unsqueeze(0)
 
-    # Create a list for holding predicted prices
-    testPredictPlot = np.empty_like(scaler.inverse_transform(data))
-    testPredictPlot[:, :] = np.nan
-    testPredictPlot[len(trainX)+(look_back*2)+1:len(data)-1, :] = scaler.inverse_transform(predict.detach().numpy().reshape(-1,1))
-  
-    # Plotting the actual price and predicted price on the same plot
+    # Now plot the future predictions along with the historical data
+# Now plot the future predictions along with the historical data
     plt.figure(figsize=(12,6))
-    plt.plot(scaler.inverse_transform(data), label='Actual Price')
-    plt.plot(testPredictPlot, label='Predicted Price')
+    plt.plot(hist.index, scaler.inverse_transform(data), color='blue', label='Historical Prices')
+    plt.plot(hist.index[len(test_predict)+(look_back*2)+1:len(data)-1], test_predict, color='green', label='Predicted Prices')
 
-    # Creating the forecast for the next 26 weeks
-    forecastPlot = np.empty((len(data)+future_weeks,1))
-    forecastPlot[:, :] = np.nan
-    forecastPlot[-future_weeks:] = forecast
+    # Generate future dates for the predictions
+    future_dates = pd.date_range(start=hist.index[-1], periods=future_weeks+1, freq='W')[1:]
 
-    plt.plot(forecastPlot, label='Forecast')
-    plt.title("Weekly Closing Prices & Forecast for next " + str(future_weeks) + " Weeks")
-    plt.xlabel('Weeks')
-    plt.ylabel('Close Price')
+    # Plot future predictions
+    plt.plot(future_dates, scaler.inverse_transform(future_pred.reshape(-1,1)), color='red', label='Simulated Future Prices')
+
+    plt.title("Historical, Predicted, and Simulated Future Prices")
+    plt.xlabel('Date')
+    plt.ylabel('Prices')
     plt.legend()
     plt.show()
+`
+
 
 if __name__ == "__main__":
     main()
